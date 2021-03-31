67afdbb40886edcadcdcde6e82375f863ee49562
mor <mor6686@gmail.com>
2021-04-01 00:03:08 +0300

    react_21.2_play_and_pause_video

--- react/ac_copy/src/component/app/App.js
+++ react/ac_copy/src/component/app/App.js
@@ -1,28 +1,29 @@
-import Input from './Input';
 import React, { useState, useEffect, useRef } from 'react'
 
 
+
 const App = () => {
+    const myRef = useRef();
 
-    const [isClicked, setIsClicked] = useState(false);
-    const inputRef = useRef(null)
 
-    useEffect(() => {
-        if (isClicked) {
-            inputRef.current.focus()
-        }
-    }, [isClicked])
-
-    const displayWord = () => {
-        setIsClicked(!isClicked);
-
+    const onPlay = () => {
+        myRef.current.play()
     }
+    const onStop = () => {
 
+        myRef.current.pause()
+    }
     return (
-        <div>
-            <button onClick={displayWord}>{isClicked ? "Save" : "Edit"}</button>
-            {isClicked && <Input setRef={inputRef} />}
+        <div style={{ width: "500px" }}>
+            <video ref={myRef} width="500" height="240" >
+                <source src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4" />
+            </video>
+            <div style={{ textAlign: 'center' }}>
+                <button onClick={onPlay}>Play</button>
+                <button onClick={onStop}>Stop</button>
+            </div>
         </div>
+
     )
 
 }
--- react/ac_copy/src/component/app/Input.js
+++
@@ -1,7 +0,0 @@
-// import React, { useState, useEffect, useRef } from 'react'
-
-const Input = ({ setRef }) => {
-    return (<input ref={setRef}></input>)
-}
-
-export default Input;
